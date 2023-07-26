502eccbe73d9f621b76c6b71f0e9b0ba02d650dd
Kelvin <Kelzjr56@gmail.com>
2023-07-26 15:34:30 -0400

    Created condition variable directory with wait_group class

---
+++ condition_variable_lock/wait_group.py
@@ -0,0 +1,28 @@
+from threading import Condition
+
+
+class WaitGroup:
+
+    def __init__(self):
+        self.wait_count = 0
+        self.cv = Condition()
+
+    def add(self, count):
+        self.cv.acquire()
+        self.wait_count += count
+        self.cv.release()
+
+    def done(self):
+        self.cv.acquire()
+        if self.wait_count > 0:
+            self.wait_count -= 1
+
+        if self.wait_count == 0:
+            self.cv.notify_all()
+        self.cv.release()
+
+    def wait(self):
+        self.cv.acquire()
+        while self.wait_count > 0:
+            self.cv.wait()
+        self.cv.release()
