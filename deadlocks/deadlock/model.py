4be3bdb67c99b11833f433d4412076b6700b4e51
Kelvin <Kelzjr56@gmail.com>
2023-07-27 14:35:10 -0400

    Created deadlock dir and added model for low_orbit_satellite_array

---
+++ deadlocks/deadlock/model.py
@@ -0,0 +1,18 @@
+class LowOrbitSatelliteArray:
+    def __init__(self, uid, path_length, front_sensor):
+        self.uid = uid
+        self.path_length = path_length
+        self.front_sensor = front_sensor
+
+
+class Intersection:
+    def __init__(self, uid, mutex, locked_by):
+        self.uid = uid
+        self.mutex = mutex
+        self.locked_by = locked_by
+
+
+class Crossing:
+    def __init__(self, position, intersection):
+        self.position = position
+        self.intersection = intersection
